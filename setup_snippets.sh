1d221659e991b5a58822095f417a4e2b18278c7c
forestfsl <756613301@11.com>
2018-06-19 15:49:40 +0800

    init

--- setup_snippets.sh
+++ setup_snippets.sh
@@ -1,6 +1,9 @@
-#/bin/bash
+#! /bin/bash
 mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
 
+# rm ~/Library/Developer/Xcode/UserData/CodeSnippets
+
 SRC_HOME=`pwd`
-ln -s $(SRC_HOME)/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
 echo "done"
+
